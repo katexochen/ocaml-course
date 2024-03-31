eb041526e0fabea64a020a483f82bfb020698ff0
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-03-31 11:59:56 +0200

    flake: first simple shell with ocaml, utop
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        buildInputs = (with pkgs; [
+          ocaml
+        ]) ++ (with pkgs.ocamlPackages; [
+          utop
+        ]);
+      };
+    });
+}
